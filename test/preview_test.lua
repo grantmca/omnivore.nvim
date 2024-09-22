d8f758a285d75f536d6a25ef21e7c1c24d4f8f99
Grant <grantmca2221@gmail.com>
2024-09-21 20:45:43 -0500

    Add test for foramtting over all note

--- test/preview_test.lua
+++ test/preview_test.lua
@@ -1,16 +1,45 @@
 describe("Text Formatting Tests", function()
 
-  local format_width
+  local formatter
 
   before_each(function()
-    format_width = require('omnivore.preview_formatter')._format_width
+    formatter = require('omnivore.preview_formatter')
+  end)
+
+  it("formats a note correctly", function()
+    local annotation = "Notes are a good way to keep track of things"
+    local quote = "The Importance of keeping notes"
+    local description = "This is an article about keeping notes and note taking"
+    local url = "www.notes.test"
+
+    local formatted_notes = formatter.notes(annotation, quote, description, url, 30)
+
+    local expected_output = {
+      '# Note:',
+      '',
+      'Notes are a good way to keep',
+      'track of things',
+      '',
+      '# Quote:',
+      'The Importance of keeping',
+      'notes',
+      '',
+      '# Library Item Description*',
+      'This is an article about',
+      'keeping notes and note taking',
+      '',
+      '# Item URL:',
+      'www.notes.test',
+    }
+
+    assert.are.same(expected_output, formatted_notes)
   end)
 
   it("splits simple sentences correctly", function()
     local input_string = "Lua is great for string manipulation"
     local width = 10
     local expected_output = {"Lua is", "great for", "string", "manipulati", "on"}
-    local result = format_width(input_string, width)
+    local result = formatter._format_width(input_string, width)
     assert.are.same(expected_output, result)
   end)
 
@@ -18,7 +47,7 @@ describe("Text Formatting Tests", function()
     local input_string = "A particularlylongword and normal words"
     local width = 8
     local expected_output = {"A", "particul", "arlylong", "word and", "normal", "words"}
-    local result = format_width(input_string, width)
+    local result = formatter._format_width(input_string, width)
     assert.are.same(expected_output, result)
   end)
 
@@ -26,7 +55,7 @@ describe("Text Formatting Tests", function()
     local input_string = "Short words but a wider width"
     local width = 20
     local expected_output = {"Short words but a", "wider width"}
-    local result = format_width(input_string, width)
+    local result = formatter._format_width(input_string, width)
     assert.are.same(expected_output, result)
   end)
 
@@ -34,7 +63,7 @@ describe("Text Formatting Tests", function()
     local input_string = ""
     local width = 10
     local expected_output = {}
-    local result = format_width(input_string, width)
+    local result = formatter._format_width(input_string, width)
     assert.are.same(expected_output, result)
   end)
 
@@ -42,7 +71,7 @@ describe("Text Formatting Tests", function()
     local input_string = "superlongword evenlongerword longestwordyet"
     local width = 5
     local expected_output = {"super", "longw", "ord", "evenl", "onger", "word", "longe", "stwor", "dyet"}
-    local result = format_width(input_string, width)
+    local result = formatter._format_width(input_string, width)
     assert.are.same(expected_output, result)
   end)
 end)
