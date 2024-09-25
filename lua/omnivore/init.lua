0b4850b2d87587786caeac40e4ff9ff14ed0cd47
Grant <grantmca2221@gmail.com>
2024-09-24 22:04:32 -0500

    small change so they dont have to pass in anything

--- lua/omnivore/init.lua
+++ lua/omnivore/init.lua
@@ -1,14 +1,15 @@
 local M = {}
 
 M.setup = function(config)
-  if config ~= nil then
-    if config.api_token ~= nil then
-      M.config.api_token = config.api_token
-    elseif os.getenv('OMNIVORE_KEY') ~= nil then
-      M.config.api_token = os.getenv('OMNIVORE_KEY')
-    else
-      print("API token is required!")
-    end
+  if not config then
+    config = {}
+  end
+  if config.api_token ~= nil then
+    M.config.api_token = config.api_token
+  elseif os.getenv('OMNIVORE_KEY') ~= nil then
+    M.config.api_token = os.getenv('OMNIVORE_KEY')
+  else
+    print("API token is required!")
   end
 end
 
